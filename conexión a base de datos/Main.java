3ba8aa56b9a9948fd446f277e2aef3c247f75ae8
Tristán Javier Cruz Hernández <124111638+TristanHDZ1@users.noreply.github.com>
2023-05-27 23:57:35 -0600

    Add files via upload

---
+++ conexión a base de datos/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.example.controlador.ControladorLibro;
+import org.example.modelo.Libro;
+import org.example.persistencia.LibroDAO;
+import org.example.vista.VentanaLibro;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaLibro view = new VentanaLibro("MvcLibros");
+        ControladorLibro conttroller =  new ControladorLibro(view);
+    }
+}
